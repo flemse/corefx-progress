574a268ff3638b6aad2031aa3500a67c1a4165b2
Immo Landwerth <immol@microsoft.com>
2015-04-09 19:59:22 -0700

    Add fetch step to publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -1,15 +1,28 @@
+ # Update our copy of corefx-progress
+
+ git fetch origin master
+ git merge origin/master --ff-only
+
+# Update the binaries and the diffs
+
 .\update.ps1
 
+# Compute whether the effective diff has changed.
+
 $availabilityHasChanged = -not [string]::IsNullOrEmpty( (git diff --name-only -- src-diff) )
+
+# Now reset or commit+push.
 
 if (-not $availabilityHasChanged)
 {
     # The effective diff hasn't changed. Reset all pending changes.
+    echo 'Nothing changed -- resetting...'
     git reset --hard
 }
 else
 {
     # The effective diff has changed, so we'll commit & push.
+    echo 'Updating official copy...'
     git add -A
     git commit -m 'Update progress'
     git push origin master
